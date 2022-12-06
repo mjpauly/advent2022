494d74719d67e9521a895c7db4ea2b896858eb43
Matthew Pauly <mjpauly@mac.com>
2022-12-05 23:07:22 -0800

    day 5 in rust

---
+++ src/day05.rs
@@ -0,0 +1,65 @@
+// #![allow(warnings)]
+
+pub fn run(input: &str) {
+    let lines: Vec<_> = input.lines().collect();
+
+    // determine placement of first blank line
+    let mut blank_idx = 0;
+    for (i, line) in lines.iter().enumerate() {
+        if line.len() == 0 {
+            blank_idx = i;
+        }
+    }
+
+    // build up stacks with vectors
+    let num_stacks = lines[blank_idx - 2].split(" ").collect::<Vec<_>>().len();
+    // front of vectors will be the bottom of the stack
+    // push/pop will be from the end of the vector
+    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); num_stacks];
+    for i in (0..(blank_idx - 1)).rev() {
+        let line = lines[i];
+        let chunks = line
+                        .chars()
+                        .collect::<Vec<char>>()
+                        .chunks(4)
+                        .map(|c| c.iter().collect::<String>())
+                        .collect::<Vec<String>>();
+        for i in 0..num_stacks {
+            let topush = chunks[i].chars().nth(1).unwrap();
+            if topush != ' ' {
+                stacks[i].push(topush);
+            }
+        }
+    }
+
+    let mut stacks2 = stacks.clone();  // for part 2
+
+    // follow crane operations
+    for line in lines[(blank_idx + 1)..].iter() {
+        let words = line.split(" ").collect::<Vec<_>>();
+        let n_to_move = words[1].parse::<usize>().unwrap();
+        let src = words[3].parse::<usize>().unwrap() - 1;
+        let dst = words[5].parse::<usize>().unwrap() - 1;
+        for _i in 0..n_to_move {  // pt1
+            let c = stacks[src].pop().unwrap();
+            stacks[dst].push(c);
+        }
+        // pt2
+        let stack_height = stacks2[src].len();
+        let mut slice = stacks2[src].split_off(stack_height - n_to_move);
+        stacks2[dst].append(&mut slice);
+    }
+
+    // print out the crates on top at the end
+    print!("Crates on top (pt1): ");
+    print_top_stacks(&stacks, num_stacks);
+    print!("Crates on top (pt2): ");
+    print_top_stacks(&stacks2, num_stacks);
+}
+
+fn print_top_stacks(stacks: &Vec<Vec<char>>, num_stacks: usize) {
+    for i in 0..num_stacks {
+        print!("{}", stacks[i].last().unwrap());
+    }
+    println!();
+}
