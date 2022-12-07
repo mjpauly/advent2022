0be16345b8f761fe3cf88b903fbcd5dff73a8a17
Matthew Pauly <mjpauly@mac.com>
2022-12-06 20:52:44 -0800

    day 6

---
+++ src/day06.rs
@@ -0,0 +1,25 @@
+// #![allow(warnings)]
+
+pub fn run(input: &str) {
+    println!("pt1: {}", find_marker(input, 4));
+    println!("pt1: {}", find_marker(input, 14));
+}
+
+fn find_marker(line: &str, marker_len: usize) -> usize {
+    for i in marker_len..line.len() {
+        let substr = &line[(i-marker_len)..i];
+        if all_unique(substr) {
+            return i;
+        }
+    }
+    0
+}
+
+fn all_unique(line: &str) -> bool {
+    for j in 0..(line.len()) {
+        if line.matches(line.chars().nth(j).unwrap()).count() > 1 {
+            return false;
+        }
+    }
+    true
+}
